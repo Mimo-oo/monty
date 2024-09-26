31189048fe8d7852076535594886b435574d9cc9
mimo-oo <fadipemichael6@gmail.com>
2024-09-26 15:32:08 -0700

    create nodes

--- main.c
+++ main.c
@@ -19,3 +19,22 @@ int main(int argc, char *argv[])
 	free_nodes();
 	return (0);
 }
+
+/**
+ * create_node - Creates a node.
+ * @n: Number to go inside the node.
+ * Return: Upon sucess a pointer to the node. Otherwise NULL.
+ */
+stack_t *create_node(int n)
+{
+	stack_t *node;
+
+	node = malloc(sizeof(stack_t));
+	if (node == NULL)
+		err(4);
+	node->next = NULL;
+	node->prev = NULL;
+	node->n = n;
+	return (node);
+}
+
